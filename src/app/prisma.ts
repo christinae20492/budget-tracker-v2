69205a69cc2c42c7253ce3e2a8d445c83121e22e
christinae20492 <125934369+christinae20492@users.noreply.github.com>
2025-07-02 00:10:09 -0400

    1751429388676

--- src/app/prisma.ts
+++ src/app/prisma.ts
@@ -1,17 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+	var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
-  }
-  prisma = (global as any).prisma;
-}
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
