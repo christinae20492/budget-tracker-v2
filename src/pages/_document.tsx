34244200d3af60b0b2d6ec7e766917eebfbecdc7
christinae20492 <125934369+christinae20492@users.noreply.github.com>
2025-07-30 12:23:30 -0400

    new icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
         <Head>
           <link rel='icon' href='/favicon.ico' type='image/x-icon' />
           <link rel="icon" href="/icon.png" type="image/png" />
-          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="icon" href="/icon-01.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="manifest" href="/site.webmanifest" />
         </Head>
